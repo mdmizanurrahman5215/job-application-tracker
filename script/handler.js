16c99c1deb3dd77eee32c0993335aa6f66620afb
Mizanur Rahman <mdmizanurrahman5215@gmail.com>
2026-02-23 22:00:59 +0600

    add delete functinality

--- script/handler.js
+++ script/handler.js
@@ -1,23 +1,27 @@
-
-function buttonHandler(e){
-    const card = e.target.closest(".card")
-    const cardId = card.dataset.id
+function buttonHandler(e) {
+  const card = e.target.closest(".card");
+  const cardId = Number(card.dataset.id)
   const interviewBtn = e.target.closest(".interview-btn");
   const rejectBtn = e.target.closest(".rejected-btn");
   const deleteBtn = e.target.closest(".card-delete-btn");
-  
-  if(!interviewBtn && !rejectBtn && !deleteBtn) return
 
-   const selectedCard = cards.find((card)=>card.id == cardId)
-   console.log(selectedCard);
+  if (!interviewBtn && !rejectBtn && !deleteBtn) return;
 
-   if(interviewBtn){
-        selectedCard.status = "Interview"
-        selectedCard.statusColor = "bg-green-500 text-black font-medium"
-   }
-   if(rejectBtn){
-        selectedCard.status = "Rejected"
-        selectedCard.statusColor = "bg-red-500 text-black font-medium"
-   }
-   renderByTab()
+  const selectedCard = cards.find((card) => card.id == cardId);
+
+//  interview 
+  if (interviewBtn) {
+    selectedCard.status = "Interview";
+    selectedCard.statusColor = "bg-green-500 text-black font-medium";
+  }
+//   reject 
+  if (rejectBtn) {
+    selectedCard.status = "Rejected";
+    selectedCard.statusColor = "bg-red-500 text-black font-medium";
+  }
+//   delete 
+  if(deleteBtn){
+    cards = cards.filter((c)=>c.id !== cardId)    
+  }
+  renderByTab();
 }
